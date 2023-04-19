805835a33767256d95b126f83be8b7c044d7b424
MatthewCheuk221 <80976897+MatthewCheuk221@users.noreply.github.com>
2023-04-19 12:04:46 +0800

    Add files via upload

---
+++ COMP3334_Workshop_1.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <TlHelp32.h>
+#include <string>
+#include <cstdint>
+#include <fstream>
+using namespace std;
+DWORD pid;
+DWORD Temp = (int)0;
+int NumValue;
+int main() {
+	HWND hWnd = FindWindowA(0, ("WannaCry V1.0"));
+	GetWindowThreadProcessId(hWnd, &pid);
+	HANDLE pHandle = OpenProcess(PROCESS_VM_READ, FALSE, pid);
+	/* Prompt the user to enter the value of n, obtained in Step 1 */
+	/* Assign n to a variable */
+	//"140737488355327" is the end of memory address.
+	int n = 0;
+	int e = 0;
+	cout << "Input n: ";
+	cin >> n;
+	cout << "Input e: ";
+	cin >> e;
+	int o = n;
+	int p = 0,q = 0;
+	while (Temp != 140737488355327) {
+		ReadProcessMemory(pHandle, (LPVOID)Temp, &NumValue, sizeof(NumValue), 0);
+		if (o % 2 == 0) {
+			p = 2;
+			while (o % 2 == 0) {
+				o = o / 2;
+			}
+		}
+		for (int i = 3; i <= sqrt(o); i += 2) {
+			while (o % i == 0) {
+				p = i;
+				o = o / i;
+			}
+		}
+		if (o > 2)
+			p = o;
+		/* NumValue is the value read from a memory address */
+		/* Check and print out the values that can be p or q such that n = pq */
+		q = n / p;
+		cout << "Largest prime factors of " << n
+			<< " are " << p << " and " << q << "\n";
+		break;
+		Temp += 1;
+	}
+	int x = (p - 1) * (q - 1);
+	int phi = x;
+	int c = 0, d = 1;
+
+	if (x == 1)
+		d = 0;
+
+	while (e > 1) {
+		// q is quotient
+		int q = e / x;
+		int t = x;
+		// Euclid's algo
+		x = e % x, e = t;
+		t = c;
+
+		// Update y and x
+		c = d - q * c;
+		d = t;
+		cout << "t: " << t << " d: " << d << " e: " << e << " q: " << q << " c: " << c << " x: " << x << "\n";
+	}
+
+	// Make d positive
+	if (d < 0)
+		d += phi;
+
+	cout << "Modular multiplicative inverse is: " << d << "\n";
+
+	system("pause");
+}
---
+++ extended_euclidean.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <tuple>        // std::tuple, std::make_tuple, std::tie
+using namespace std;
+
+// Recursive function to demonstrate the extended Euclidean algorithm.
+// It returns multiple values using tuple in C++.
+tuple<int, int, int> extended_gcd(int a, int b)
+{
+    if (a == 0) {
+        cout << "a: " << a << " b: " << b << "\n" << endl;
+        return make_tuple(b, 0, 1);
+    }
+
+    int gcd, x, y;
+
+    // unpack tuple returned by function into variables
+    tie(gcd, x, y) = extended_gcd(b % a, a);
+    cout << "y: " << y << " gcd: " << gcd << " b: " << b << " x: " << x << " a: " << a << "\n" << endl;
+    return make_tuple(gcd, (y - (b / a) * x), x);
+}
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+    cout << "Input a: ";
+    cin >> a;
+    cout << "Input b: ";
+    cin >> b;
+    tuple<int, int, int> t = extended_gcd(a, b);
+
+    int gcd = get<0>(t);
+    int x = get<1>(t);
+    int y = get<2>(t);
+
+    cout << "y: " << y << " gcd: " << gcd << " b: " << b << " x: " << x << " a: " << a << "\n" << endl;
+    cout << "1 = " << a << " * " << x << " + " << b << " * " << y << "\n" << endl;
+    cout << "Inverse: " << x << endl;
+ 
+
+    return 0;
+}
